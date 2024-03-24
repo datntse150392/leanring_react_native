824eab1c8f135d57c8fe1ecc9877a689b08361e6
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-03-24 20:10:25 +0700

    React Native Tutorial - 12: Button

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,60 +1,15 @@
-import { View, Text, Image, ImageBackground, ScrollView } from "react-native";
+import { View, Button } from "react-native";
 
 const Logo = require("./assets/icon.png");
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <ScrollView>
-        <Image
-          source={{ uri: "https://picsum.photos/id/1/200/300" }}
-          style={{ width: "100%", height: 200 }}
-        ></Image>
-        <Text>
-          HOT】MỪNG 1 THÁNG RA MẮT - ĐUA TOP PHÚ GIÁP THIÊN HẠ - ĐĂNG NHẬP
-          LANDING - SĂN NGAY QUÀ XỊN🎉🎉 🔥THAM GIA NGAY TẠI:
-          https://vangioipk.vn/nhan-qua 🕐Thời Gian: diễn ra trong vòng 14 ngày
-          từ ngày 21/03 -> 23h59 ngày 03/04. 💥EVENT 1: ĐIỂM DANH HÀNG NGÀY - CÓ
-          NGAY CODE XỊN💥 - Đăng nhập mỗi ngày nhận 1 lượt Điểm Danh, nhấn vào
-          Ngày hiện tại để điểm danh thành công. - Không đăng nhập ngày nào sẽ
-          mất lượt ngày đó, ngày tiếp theo đăng nhập phải điểm danh bù lần lượt,
-          yêu cầu đăng nhập hàng ngày. - Lượt Điểm Danh được làm mới hàng ngày
-          nên sau khi đăng nhập nhận lượt yêu cầu Điểm Danh luôn không được tích
-          lại. - Điểm Danh hàng ngày đạt đủ số ngày yêu cầu sẽ nhận được
-          GiftCode 🎁Phần Thưởng: ✅Mốc 3 ngày: Lệnh Chiêu Mộ Cao *20, Tàng Bảo
-          Đồ Cao *1 ✅Mốc 7 ngày: Thẻ Chiêu Mộ Phe *10, Thẻ Triệu Hồi Tọa Kỵ *5,
-          Thẻ Triệu Hồi Thánh Vật *5 ✅Mốc 14 ngày: Mảnh Thần Binh *50, Gói Nâng
-          Sao Truyền Thuyết *2, Đột Phá Đan *500, Đá Cường Hóa *500 💥EVENT 2:
-          ĐUA TOP PHÚ GIÁP THIÊN HẠ💥 🛎Thể Lệ 🔸Trong thời gian diễn ra sự kiện,
-          các Tiên Quân sẽ thi đấu để trở thành Phú Giáp Thiên Hạ thông qua việc
-          nạp Sohacoin vào game, mức Xếp Hạng sẽ được tính bằng tổng SOHACOIN
-          được quy đổi vào game Vạn Giới PK theo từng Nhân Vật và Server mà Tiên
-          Quân chơi. 🔸Điểm Phú Hào được tính như sau: 1 SohaCoin = 1 Điểm Phú
-          Hào Top 10 Tiên Quân có số Điển Phú Hào cao nhất sẽ nhận được quà từ
-          sự kiện Phú Giáp Thiên Hạ. 🎯Phạm Vi Áp Dụng: Toàn bộ các server
-          🎁PHẦN THƯỞNG: 🏆TOP 1: Tôn Ngộ Không *2, Rương Chọn Tử Nữ Chí Tôn *1,
-          Chiến Hồn Lệnh *30, Mảnh Thần Binh *200, Gói Chọn Nguyên Tinh Thần Khí
-          *50 🥈TOP 2: Tôn Ngộ Không *1, Gói Chọn Thánh Vật-Cao *1, Chiến Hồn
-          Lệnh *20, Mảnh Thần Binh *100, Gói Chọn Nguyên Tinh Thần Khí *20 🥉TOP
-          3: Gói Chọn Tướng Hắc Kim *2, Chiến Hồn Lệnh *10, Mảnh Thần Binh *50,
-          Gói Nguyên Liệu Sơ *2 🏅TOP 4 - 6: Gói Chọn Tướng Hắc Kim *1, Chiến
-          Hồn Lệnh *10, Mảnh Thần Binh *50, Gói Nguyên Liệu Sơ *1 🎖TOP 7 - 10:
-          Gói Chọn Tướng Trác Việt *1, Thẻ Triệu Hồi Tọa Kỵ *10 , Mảnh Thần Binh
-          *50, Gói Nguyên Liệu Sơ *1 ⚠️ LƯU Ý: - Tiên Quân nạp từ 1000 Linh Ngọc
-          trở lên mới được tính tham gia sự kiện và Top Phú Giáp Thiên Hạ. - Xếp
-          hạng được tính theo Nhân Vật và Server mà Tiên Quân đã quy đổi
-          SohaCoin thành Linh Ngọc vào game, KHÔNG tính theo tài khoản đăng
-          nhập. - Những Tiên Quân có số điểm phú hào ngang nhau, thì Tiên Quân
-          đạt được mức điểm đó trước sẽ có thứ hạng cao hơn Tiên Quân còn lại. -
-          Phải quy đổi SohaCoin thành Linh Ngọc vào game mới được tính, chỉ nạp
-          SohaCoin mà ko dùng đổi vào game cũng sẽ không được tính điểm. - Quà
-          Top 10 sẽ được trao qua thư sau khi kết thúc sự kiện ( chậm nhất trong
-          vòng 72h, không tính thứ 7 và chủ nhật). - Sự kiện áp dụng và tính kết
-          quả với tất cả Tiên Quân trên toàn bộ các server của Vạn Giới PK. -
-          Tiêu Linh Ngọc sẽ không được tính vào điểm phú hào. 😍 Khi Tiên Quân
-          đạt VIP 12, hãy chủ động tải App SohaGame - CSKH và chat với GM để
-          được hỗ trợ nhận hỗ trợ từ GM ! 😍
-        </Text>
-      </ScrollView>
+      <Button
+        title="Press"
+        onPress={() => console.log("Press")}
+        color="yellow"
+        disabled
+      />
     </View>
   );
 }
