93e47849c2d408ac6eae7e92930e8836418f2ffa
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-03-24 20:52:39 +0700

    React Native Tutorial - 16: Activity Indicator

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
-import { View, StatusBar } from "react-native";
+import { View, StatusBar, ActivityIndicator, Button } from "react-native";
 const Logo = require("./assets/icon.png");
 export default function App() {
-  const [isVisableModal, setIsVisableModal] = useState(false);
+  const [loading, setLoading] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <StatusBar hidden />
+      <Button
+        onPress={() => setLoading(true)}
+        title="Display Activity Indicator"
+      />
+      <ActivityIndicator size={"large"} animating={loading}></ActivityIndicator>
     </View>
   );
 }
