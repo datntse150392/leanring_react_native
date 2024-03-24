8e9ea8c48e509e26771f0ee89c38ce6d77a45d98
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-03-24 20:38:03 +0700

    React Native Tutorial - 15: Status Bar

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,23 +1,11 @@
 import { useState } from "react";
-import { View, Image, Text, Modal, Button } from "react-native";
+import { View, StatusBar } from "react-native";
 const Logo = require("./assets/icon.png");
 export default function App() {
   const [isVisableModal, setIsVisableModal] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button title="Open Modal" onPress={() => setIsVisableModal(true)} />
-      <Modal
-        visible={isVisableModal}
-        onRequestClose={() => setIsVisableModal(false)}
-        animationType="slide"
-        presentationStyle="pageSheet"
-      >
-        <View>
-          <Image source={Logo} style={{ width: 200, height: 200 }} />
-          <Text>Image Gallery</Text>
-          <Button title="Closed" onPress={() => setIsVisableModal(false)} />
-        </View>
-      </Modal>
+      <StatusBar hidden />
     </View>
   );
 }
