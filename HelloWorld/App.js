cfda78fe0d49ff6a5b3a12831901eba2864e18e7
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-03-25 00:30:56 +0700

    React Native Tutorial - 28: Flex Direction

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -3,9 +3,9 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "red", flex: 1 }}>1</Box>
-      <Box style={{ backgroundColor: "yellow", flex: 2 }}>1</Box>
-      <Box style={{ backgroundColor: "green", flex: 3 }}>1</Box>
+      <Box style={{ backgroundColor: "red", flex: 1 }}>Box 1</Box>
+      <Box style={{ backgroundColor: "yellow", flex: 2 }}>Box 2</Box>
+      <Box style={{ backgroundColor: "green", flex: 3 }}>Box 3</Box>
     </View>
   );
 }
@@ -13,6 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column-reverse",
     backgroundColor: "#fff",
     marginTop: 64,
     borderWidth: 10,
