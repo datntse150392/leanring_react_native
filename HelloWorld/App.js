370af68d6203c23d38f5d02e7efba1a065903b06
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-03-24 23:30:37 +0700

    React Native Tutorials - 21: Multiple Styles

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,10 +1,32 @@
-import { View } from "react-native";
-import Greet from "./components/Greet";
+import { View, StyleSheet, Text } from "react-native";
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Greet name="World" />
-      <Greet name="Nguyen Thanh Dat" />
+    <View style={styles.container}>
+      <View style={[styles.box, styles.lightBlueBg]}>
+        <Text>Lightblue box</Text>
+      </View>
+
+      <View style={[styles.box, styles.lightGreenBg]}>
+        <Text>Lightblue box</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "plum",
+    padding: 60,
+  },
+  box: {
+    width: 100,
+    height: 100,
+  },
+  lightBlueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightGreenBg: {
+    backgroundColor: "lightgreen",
+  },
+});
