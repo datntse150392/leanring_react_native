7312ecc9a336e911737c8465539bc477d6f9fe20
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-03-24 23:23:09 +0700

    React Native Tutorial - 20: StyleSheet API

--- HelloWorld/components/Greet.js
+++ HelloWorld/components/Greet.js
@@ -1,9 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function Greet({ name }) {
   return (
-    <View>
-      <Text>Hello, {name}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello, {name}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "plum",
+    padding: 60,
+    margin: 10,
+  },
+  title: {
+    color: "red",
+  },
+});
