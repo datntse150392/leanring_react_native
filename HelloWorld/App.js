6511d3eb838c2bfb8a858ccc068d861239635a2d
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-03-24 21:14:37 +0700

    React Native Tutorial - 18: Custom Components

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,23 +1,10 @@
-import { View, Button, Alert } from "react-native";
-const Logo = require("./assets/icon.png");
+import { View } from "react-native";
+import Greet from "./components/Greet";
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button
-        title="Alert"
-        onPress={() =>
-          Alert.alert("Invalid Data!", "DOB invalid", [
-            {
-              text: "Cancel",
-              onPress: () => console.log("Cancel Pressed"),
-            },
-            {
-              text: "OK",
-              onPress: () => console.log("OK Pressed"),
-            },
-          ])
-        }
-      />
+      <Greet name="World" />
+      <Greet name="Nguyen Thanh Dat" />
     </View>
   );
 }
