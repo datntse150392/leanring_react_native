cc4fe1739644d5be6d830e03a48b0a47c4e79244
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-03-24 20:18:56 +0700

    React Native Tutorial - 13:  Pressable

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,15 +1,18 @@
-import { View, Button } from "react-native";
+import { View, Pressable, Image, Text } from "react-native";
 
 const Logo = require("./assets/icon.png");
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button
-        title="Press"
-        onPress={() => console.log("Press")}
-        color="yellow"
-        disabled
-      />
+      <Pressable onPress={() => console.log("Text Press")}>
+        <Text>Image Gallery</Text>
+      </Pressable>
+      <Pressable onPress={() => console.log("Image Press")}>
+        <Image style={{ width: "100%", height: 200 }} source={Logo}></Image>
+      </Pressable>
+      <Pressable onLongPress={() => console.log("Image onLongPress")}>
+        <Image style={{ width: "100%", height: 200 }} source={Logo}></Image>
+      </Pressable>
     </View>
   );
 }
