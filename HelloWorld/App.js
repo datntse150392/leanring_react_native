1f96c8c04fae2f0208d6b4d1ffb06e4dc2fe4ed9
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-03-24 19:58:22 +0700

    React Native Tutorial - 10: Image

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,11 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
+
+const Logo = require("./assets/icon.png");
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "#fff", padding: 60 }}>
-      <Text>
-        <Text style={{ color: "#d9d9d9" }}>Hello </Text>
-        World
-      </Text>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      {/* <Image source={Logo} style={{ width: 200, height: 200 }}></Image>
+      <Image
+        source={{ uri: "https://picsum.photos/id/1/200/300" }}
+        style={{ width: 200, height: 200 }}
+      ></Image> */}
+      <ImageBackground
+        source={{ uri: "https://picsum.photos/id/1/200/300" }}
+        style={{ flex: 1 }}
+      ></ImageBackground>
     </View>
   );
 }
