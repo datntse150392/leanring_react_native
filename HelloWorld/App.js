9c950fa0f3e033c73f98355f75560b6dd4dabdcc
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-03-25 00:41:39 +0700

    React Native Tutorial - 30: Align Items

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -3,7 +3,7 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "red" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "red", paddingVertical: 100 }}>Box 1</Box>
       <Box style={{ backgroundColor: "yellow" }}>Box 2</Box>
       <Box style={{ backgroundColor: "green" }}>Box 3</Box>
     </View>
@@ -12,9 +12,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "space-evenly",
+    flexDirection: "row",
     backgroundColor: "#fff",
+    alignItems: "baseline",
     marginTop: 64,
     borderWidth: 10,
     borderColor: "blue",
