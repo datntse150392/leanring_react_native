fe4c74400727dfc1210fc8006c5bff000e743887
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-03-25 11:40:24 +0700

    React Native Tutorial - 32: Flex Wrap

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -3,15 +3,20 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "red", alignSelf: "flex-end" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "red" }}>Box 1</Box>
       <Box style={{ backgroundColor: "yellow" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "green" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 7</Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    height: 300,
+    flexWrap: "wrap",
     flexDirection: "column",
     backgroundColor: "#fff",
     alignItems: "baseline",
