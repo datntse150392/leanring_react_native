fd91c16b4e4bedfcec76fcf48785ae29f2897b2c
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-03-26 21:23:36 +0700

    React Native Tutorial - 35: Flex Basis

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -3,8 +3,8 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "red" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "yellow" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "red", flexBasis: 140 }}>Box 1</Box>
+      <Box style={{ backgroundColor: "yellow", height: 140 }}>Box 2</Box>
       <Box style={{ backgroundColor: "green" }}>Box 4</Box>
       <Box style={{ backgroundColor: "green" }}>Box 5</Box>
       <Box style={{ backgroundColor: "green" }}>Box 6</Box>
