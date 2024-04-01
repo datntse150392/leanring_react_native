590eea6d6815f18f0bdb64b4b05744d6a667e534
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-04-01 22:40:07 +0700

    React Native Tutorial - 36: Flex Shrink

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -3,28 +3,22 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "red", flexBasis: 140 }}>Box 1</Box>
-      <Box style={{ backgroundColor: "yellow", height: 140 }}>Box 2</Box>
-      <Box style={{ backgroundColor: "green" }}>Box 4</Box>
-      <Box style={{ backgroundColor: "green" }}>Box 5</Box>
-      <Box style={{ backgroundColor: "green" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "green" }}>Box 7</Box>
+      <Box style={{ backgroundColor: "red", flexShrink: 1 }}>Box 1 shrink</Box>
+      <Box style={{ backgroundColor: "yellow", flexShrink: 1 }}>
+        Box 2 shink
+      </Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: 300,
-    flexWrap: "wrap",
-    alignContent: "center",
-    flexDirection: "column",
-    backgroundColor: "#fff",
-    alignItems: "baseline",
+    flex: 1,
+    width: 200,
     marginTop: 64,
     borderWidth: 10,
     borderColor: "blue",
-    rowGap: 20,
-    columnGap: 20,
+    flexDirection: "row",
+    justifyContent: "flex-start",
   },
 });
