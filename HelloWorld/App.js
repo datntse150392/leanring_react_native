7d5cfd7a094521008723f21f5fef6e65722950e4
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-04-02 22:44:04 +0700

    React Native Tutorial - 52: FlatList
    - Giả sử bạn dùng hàm map trong js thì nó sẽ render tất cả dữ liệu luôn cho bạn => tốn thời gian khúc đầu để get data
    - Dùng FlatList sẽ lazy loading data, scroll tới đâu sẽ load tới đó

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -4,20 +4,29 @@ import {
   SafeAreaView,
   Platform,
   ScrollView,
+  FlatList,
 } from "react-native";
 import PokemonCard from "./components/PokemonCard";
 import data from "./data.json";
 export default function App() {
   return (
     <SafeAreaView style={styles.safeAreaView}>
-      <ScrollView>
+      {/* <ScrollView>
         <View style={styles.container}>
           {data &&
             data.pokemon.map((pokemon) => {
               return <PokemonCard key={pokemon.name} {...pokemon} />;
             })}
         </View>
-      </ScrollView>
+      </ScrollView> */}
+      <FlatList
+        data={data.pokemon}
+        renderItem={({ item }) => {
+          return <PokemonCard key={item.name} {...item} />;
+        }}
+        // KeyExtractor is a function that takes an item and returns a unique key for that item.
+        keyExtractor={(item) => item.name}
+      />
     </SafeAreaView>
   );
 }
