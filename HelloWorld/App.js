40d72587d06fa8555467e8f59cf85fd8fd70f5a4
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-04-05 17:11:39 +0700

    React Native Tutorial - 81: Tab Navigation Options

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -2,14 +2,30 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from "./components/home";
 import { About } from "./components/about";
+import { Text } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "red",
+          tabBarInactiveTintColor: "blue",
+          tabBarLabelPosition: "beside-icon",
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: () => <Ionicons name="person" size={20} />,
+            tabBarBadge: 3,
+          }}
+        />
         <Tab.Screen name="About" component={About} />
       </Tab.Navigator>
     </NavigationContainer>
