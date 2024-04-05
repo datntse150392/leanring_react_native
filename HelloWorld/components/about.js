44ab691184fbd67e9b1c4ff3098059ed8c9cb77e
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-04-05 13:45:08 +0700

    React Native Tutorial - 74: Navigation between Screens
    - Nên dùng useNagivate hơn là dùng prop

--- HelloWorld/components/about.js
+++ HelloWorld/components/about.js
@@ -1,9 +1,12 @@
-import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, Button } from "react-native";
 export function About() {
+  const navigation = useNavigation();
   return (
     <View>
       <Text>About Screen</Text>
       <Text>This is a simple React component.</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
     </View>
   );
 }
