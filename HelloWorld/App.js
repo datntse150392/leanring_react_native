a879097d00d85bfc54c45d0c60651443fb5b6127
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-04-05 14:07:45 +0700

    React Native Tutorial - 76: Stack Navigation Options
    - Ở bài này thì chúng ta học được cách là điều chỉnh style cho bên phía Header

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -2,12 +2,29 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "./components/home";
 import { About } from "./components/about";
+import { Pressable, Text } from "react-native";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="About">
+      <Stack.Navigator
+        initialRouteName="About"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "lightblue",
+          },
+          headerTintColor: "black",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerRight: () => (
+            <Pressable onPress={() => alert("On press menu")}>
+              <Text style={{ color: "#000" }}>Menu</Text>
+            </Pressable>
+          ),
+        }}
+      >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen
           name="About"
